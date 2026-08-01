84a08fe3e03a2878b7c6ff6fd9c889aabe06e529
iu5vcp-byte <iu5vcp@gmail.com>
2026-08-01 19:55:29 +0200

    Add files via upload

---
+++ styles/Sentieri_monte_morello608CLaCassianaCollinatracks_22_style.js
@@ -0,0 +1,43 @@
+var size = 0;
+var placement = 'point';
+
+var style_Sentieri_monte_morello608CLaCassianaCollinatracks_22 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "11.700000000000001px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = 'left';
+    var offsetX = 8;
+    var offsetY = 3;
+    var overflow = false;
+    var repeat = 180.0;
+    var placement = 'line';
+    if (exp_label_Sentieri_monte_morello608CLaCassianaCollinatracks_22_eval_expression(context) !== null && resolution > 0 && resolution < 0) {
+        labelText = String(exp_label_Sentieri_monte_morello608CLaCassianaCollinatracks_22_eval_expression(context));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.8}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2.28}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(230,18,68,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2.28}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })];
+
+    return style;
+};
